9b3b3f7a2f1d5b5a2d358e72ab18ab6ae8387317
Raman Verma <rverma@pbl.ca>
2021-11-18 22:02:05 -0600

    Learn Command handlers

--- Marketplace/API/ClassifiedAdsCommandsApi.cs
+++ Marketplace/API/ClassifiedAdsCommandsApi.cs
@@ -1,17 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Marketplace.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using static Marketplace.Contracts.ClassifiedAds;
 
 namespace Marketplace.API
 {
     [Route("/ad")]
     public class ClassifiedAdsCommandsApi:Controller
     {
+        private readonly Func<IHandleCommand<V1.Create>> _createAdCommandHandlerFactory;
+        //private readonly IHandleCommand<V1.Create> _createAdCommandHandler;
+
+        // private readonly ClassifiedAdApplicationService _applicationService;
+
+        public ClassifiedAdsCommandsApi(Func<IHandleCommand<V1.Create>> createAdCommandHandlerFactory) 
+            => _createAdCommandHandlerFactory = createAdCommandHandlerFactory;
+
+        // public ClassifiedAdsCommandsApi(IHandleCommand<V1.Create> createAdCommandHandler) =>
+        //     _createAdCommandHandler = createAdCommandHandler;
+
+
         [HttpPost]
-        public async Task<IActionResult> Post(
-            Contracts.ClassifiedAds.V1.Create request)
-        {
-            //handle the request here
-            return Ok();
-        }
+        public Task Post(V1.Create request) 
+            => _createAdCommandHandlerFactory().Handle(request);
     }
 }
